83db919ce10a1ff97a2619e5986b7776d6a7c8a7
Vic-G <wvictoronyango@gmail.com>
2026-08-22 22:38:36 +0300

    Modification of index.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,6 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
@@ -14,7 +9,7 @@ function App() {
         <h1>A Journey in Tech</h1>
         </div>
         <div>
-          <img src="https://images.unsplash.com/photo-1489875347897-49f64b51c1f8?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8bmVyZHxlbnwwfHwwfHx8MA%3D%3D" className="Profile photo" alt="Profile photo" />
+          <img src="https://images.unsplash.com/photo-1489875347897-49f64b51c1f8?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8bmVyZHxlbnwwfHwwfHx8MA%3D%3D" className="Profile photo" alt="profile-photo" />
           <p>
             This is a short personal blog on a journey in tech.
             A narration by Victor Wasunna.
@@ -61,11 +56,9 @@ function App() {
           </ul>
         </div>
       </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
     </>
   )
 }
 
 export default App
+
